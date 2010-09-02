be55473faeb464d6e0687d0b205987e088afd2d7
bubenkoff <bubenkoff@65ff189f-35ee-0310-953c-23049635c3cf>
2010-09-02 10:22:51 +0000

    template fixes

--- src/zojax/django/extendedmenus/templatetags/extended_menu_tags.py
+++ src/zojax/django/extendedmenus/templatetags/extended_menu_tags.py
@@ -2,6 +2,7 @@ from django import template
 from django.core.urlresolvers import reverse
 from treemenus.models import MenuItem
 import re
+from zojax.django.extendedmenus.models import MenuItemExtension
 
 
 register = template.Library()
@@ -11,32 +12,39 @@ def displayed_menu_items(menu_items, request):
     user = request.user
     children = []
     for child in menu_items:
-        if user.is_anonymous() and child.extension.only_authenticated:
-            continue
-        if user.is_authenticated() and child.extension.only_anonymous:
-            continue
+        try:
+            extension = child.extension
+            if user.is_anonymous() and extension.only_authenticated:
+                continue
+            if user.is_authenticated() and extension.only_anonymous:
+                continue
+        except MenuItemExtension.DoesNotExist:
+            pass
         children.append(child)
     return children
-    
+
 register.filter('displayed_menu_items', displayed_menu_items)
 
 
 def menu_item_selected(menu_item, request):
     if not isinstance(menu_item, MenuItem):
         return False
-    selected_patterns = menu_item.extension.selected_patterns
-    for pattern in selected_patterns.splitlines():
-        if selected_patterns and re.compile(pattern).match(request.path):
-            return True
-        
+    try:
+        selected_patterns = menu_item.extension.selected_patterns
+        for pattern in selected_patterns.splitlines():
+            if selected_patterns and re.compile(pattern).match(request.path):
+                return True
+    except MenuItemExtension.DoesNotExist:
+            pass
+
     if menu_item.named_url:
-        url = reverse(menu_item.named_url) 
+        url = reverse(menu_item.named_url)
     else:
         url = menu_item.url
 
     if request.path == url:
         return True
-        
+
     return False
 
 register.filter('menu_item_selected', menu_item_selected)
